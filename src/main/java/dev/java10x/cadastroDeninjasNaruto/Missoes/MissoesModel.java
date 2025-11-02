2a9508668b886ab1ea00c3fec801b627abbdefb6
marconi-prog <marconifarias82@gmail.com>
2025-11-01 22:54:57 -0300

    fix

--- src/main/java/dev/java10x/cadastroDeninjasNaruto/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/cadastroDeninjasNaruto/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.java10x.cadastroDeninjasNaruto.Missoes;
 
 import dev.java10x.cadastroDeninjasNaruto.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
-@Table(name = "")
+@Table(name = "/tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,23 +22,4 @@ public class MissoesModel {
 
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
-
-    public MissoesModel() {
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
--- src/main/java/dev/java10x/cadastroDeninjasNaruto/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/cadastroDeninjasNaruto/Ninjas/NinjaModel.java
@@ -2,56 +2,33 @@ package dev.java10x.cadastroDeninjasNaruto.Ninjas;
 
 import dev.java10x.cadastroDeninjasNaruto.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     //criar o id e coloca um valor automaticamente com numeros
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String nome;
+
+    @Column(unique = true)
     private String email;
+
     private int idade;
 
     //varios para um
     @ManyToOne
     @JoinColumn(name = "missoes_id") //foreing key
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
